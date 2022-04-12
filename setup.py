19393e604a82c993e7abab34f55ded77bf95953f
leospinaf <leospinaf@unal.edu.co>
2022-04-12 10:20:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='moo',
     version='0.1.1',
     description='multi objective optimisation for bipartite graphs',
-    url='https://github.com/UoMResearchIT/mo-community-detection-bipartite',
+    url='https://github.com/leospinaf/BipartiteMOEA',
     author='Julia Handl/Hichem Barki',
     author_email='todo',
     license='todo',
